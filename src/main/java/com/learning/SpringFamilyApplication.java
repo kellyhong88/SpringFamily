8d7f0e53e62545545d484b6f625d17b92f509bfc
kellyhong88 <kellyhong88@163.com>
2019-05-18 16:24:40 +0800

    remove dao

--- src/main/java/com/learning/SpringFamilyApplication.java
+++ src/main/java/com/learning/SpringFamilyApplication.java
@@ -1,14 +1,13 @@
 package com.learning;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@EnableAutoConfiguration
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class SpringFamilyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringFamilyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFamilyApplication.class, args);
+    }
 }
