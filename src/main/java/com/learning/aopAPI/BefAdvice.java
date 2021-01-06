57caaac4ebef3d0c6792a1546516438505f45f25
xijun.lh <xijun.lh@antfin.com>
2021-01-06 17:48:51 +0800

    optimize

--- src/main/java/com/learning/aopAPI/BefAdvice.java
+++ src/main/java/com/learning/aopAPI/BefAdvice.java
@@ -7,8 +7,7 @@ import org.springframework.aop.MethodBeforeAdvice;
 public class BefAdvice implements MethodBeforeAdvice {
 
 	public void before(Method method, Object[] arg, Object target) throws Throwable {
-		System.out
-				.println("BeforeAdvice: method is " + method.getName() + ", target is " + target.getClass().getName());
+		System.out.println("BeforeAdvice: method is " + method.getName() + ", target is " + target.getClass().getName());
 	}
 
 }
